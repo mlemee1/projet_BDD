a7edf7d5f2c97c349da01b0bd708009a5bc45bbe
fowled <ma15fo43@gmail.com>
2023-12-17 12:08:09 +0100

    Ajout de la documentation (finale)

---
+++ interface.py
@@ -0,0 +1,241 @@
+import customtkinter as ctk
+
+
+class RepertoireUI(ctk.CTk):
+    def __init__(self, repertoire):
+        """
+        Initialise l'interface utilisateur de l'application de répertoire.
+
+        Args:
+            repertoire (Repertoire): Instance de la classe Repertoire pour gérer les opérations de la base de données.
+        """
+        super().__init__()
+
+        self.rep = repertoire
+
+        self.init_ui()
+        self.mainloop()
+
+    def init_ui(self):
+        """
+        Configure l'interface utilisateur initiale, incluant la création et le placement des widgets.
+        """
+        self.title("Répertoire")
+        self.geometry("400x350")
+        ctk.set_appearance_mode("dark")
+
+        ctk.CTkLabel(
+            self, text="Répertoire", font=ctk.CTkFont(size=20, weight="bold")
+        ).pack(padx=20, pady=(20, 0))
+
+        ctk.CTkLabel(self, text="Bienvenue dans votre répertoire.").pack(
+            padx=20, pady=10
+        )
+
+        for text, commande in [
+            ("Ajouter un contact", self.ajout_contact),
+            ("Afficher les contacts", self.afficher_contacts),
+            ("Rechercher un contact", self.chercher_contacts),
+            ("Supprimer un contact", self.supprimer_contact),
+        ]:
+            ctk.CTkButton(self, text=text, command=commande).pack(padx=20, pady=10)
+
+        self.quitter_button(self).pack(padx=20, pady=10)
+
+    def input_dialog(self, title, text) -> str:
+        """
+        Affiche une boîte de dialogue pour la saisie de l'utilisateur.
+
+        Args:
+            title (str): Le titre de la boîte de dialogue.
+            text (str): Le texte à afficher dans la boîte de dialogue.
+
+        Returns:
+            str: La saisie de l'utilisateur.
+        """
+        dialog = ctk.CTkInputDialog(text=text, title=title)
+
+        return dialog.get_input()
+
+    def ajout_contact(self):
+        """
+        Gère l'ajout d'un nouveau contact. Ouvre des boîtes de dialogue pour saisir les informations du contact.
+        """
+        nom = self.input_dialog(
+            "Nom de famille", "Entrez le nom de famille du contact :"
+        )
+
+        if not nom:
+            return
+
+        prenom = self.input_dialog("Prénom", "Entrez le prénom du contact :")
+
+        if not prenom:
+            return
+
+        telephone = self.input_dialog(
+            "Numéro de téléphone", "Entrez le numéro de téléphone du contact :"
+        )
+
+        if not telephone:
+            return
+
+        self.rep.ajouter_contact(nom, prenom, telephone)
+
+    def afficher_contacts(self):
+        """
+        Affiche une nouvelle fenêtre listant tous les contacts stockés dans la base de données.
+        """
+        fenetre_contacts = ctk.CTkToplevel()
+        fenetre_contacts.title("Affichage des contacts")
+        fenetre_contacts.geometry("400x300")
+
+        contacts = self.rep.afficher_contacts()
+
+        if len(contacts) > 0:
+            for contact in contacts:
+                ctk.CTkLabel(
+                    fenetre_contacts, text=f"{contact[0]} {contact[1]} : {contact[2]}"
+                ).pack(padx=20, pady=1)
+        else:
+            ctk.CTkLabel(
+                fenetre_contacts,
+                text="Aucun contact n'est présent dans le répertoire !",
+            ).pack(padx=20, pady=10)
+
+        self.quitter_button(fenetre_contacts).pack(padx=20, pady=10)
+        fenetre_contacts.after(100, fenetre_contacts.lift)
+
+    def chercher_contacts(self):
+        """
+        Gère la recherche de contacts. Ouvre une boîte de dialogue pour la saisie du nom puis du prénom à rechercher.
+        """
+        nom = self.input_dialog("Nom du contact", "Entrez le nom du contact")
+
+        if not nom:
+            return
+
+        prenom = self.input_dialog("Prénom du contact", "Entrez le prénom du contact")
+
+        if not prenom:
+            return
+
+        fenetre_contacts = ctk.CTkToplevel()
+        fenetre_contacts.title("Recherche de contacts")
+        fenetre_contacts.geometry("400x300")
+
+        ctk.CTkLabel(
+            fenetre_contacts,
+            text="Recherche de contacts",
+            font=ctk.CTkFont(size=20, weight="bold"),
+        ).pack(padx=20, pady=(20, 0))
+
+        contacts = self.rep.chercher_contact(nom, prenom)
+
+        if len(contacts) > 0:
+            label_text = f"Nous avons trouvé {len(contacts)} contact(s) avec le nom '{nom} {prenom}'."
+        else:
+            label_text = (
+                f"Nous n'avons pas trouvé de contact avec le nom '{nom} {prenom}'."
+            )
+
+        ctk.CTkLabel(fenetre_contacts, text=label_text).pack(padx=20, pady=10)
+
+        for contact in contacts:
+            ctk.CTkLabel(
+                fenetre_contacts, text=f"{contact[0]} {contact[1]} : {contact[2]}"
+            ).pack(padx=20, pady=1)
+
+        self.quitter_button(fenetre_contacts).pack(padx=20, pady=10)
+
+        fenetre_contacts.after(100, fenetre_contacts.lift)
+
+    def supprimer_contact(self):
+        """
+        Gère la suppression d'un contact. Ouvre des boîtes de dialogue pour confirmer la suppression d'un contact spécifique.
+        """
+        nom = self.input_dialog(
+            "Nom de famille", "Entrez le nom de famille du contact :"
+        )
+
+        if not nom:
+            return
+
+        prenom = self.input_dialog("Prénom", "Entrez le prénom du contact :")
+
+        if not prenom:
+            return
+
+        fenetre_confirmation = ctk.CTkToplevel()
+        fenetre_confirmation.title("Confirmation")
+        fenetre_confirmation.geometry("400x300")
+
+        ctk.CTkLabel(
+            fenetre_confirmation,
+            text="Suppression de contacts",
+            font=ctk.CTkFont(size=20, weight="bold"),
+        ).pack(padx=20, pady=(20, 0))
+
+        ctk.CTkLabel(
+            fenetre_confirmation,
+            text="Êtes-vous certain(e) de vouloir supprimer ce contact ?",
+        ).pack(padx=20, pady=10)
+
+        ctk.CTkButton(
+            fenetre_confirmation,
+            text="Oui",
+            command=lambda: self.confirmation_suppression(
+                nom, prenom, fenetre_confirmation
+            ),
+        ).pack(padx=20, pady=10)
+
+        ctk.CTkButton(
+            fenetre_confirmation, text="Non", command=fenetre_confirmation.destroy
+        ).pack(padx=20, pady=10)
+
+    def confirmation_suppression(
+        self, nom: str, prenom: str, fenetre_confirmation: ctk.CTkToplevel
+    ):
+        """
+        Effectue la suppression effective d'un contact après confirmation de l'utilisateur.
+
+        Args:
+            nom (str): Le nom de famille du contact à supprimer.
+            prenom (str): Le prénom du contact à supprimer.
+            fenetre_confirmation (ctk.CTkToplevel): La fenêtre de confirmation de suppression.
+        """
+        fenetre_confirmation.destroy()
+
+        fenetre_contacts = ctk.CTkToplevel()
+        fenetre_contacts.title("Suppression de contacts")
+        fenetre_contacts.geometry("400x300")
+
+        ctk.CTkLabel(
+            fenetre_contacts,
+            text="Suppression de contacts",
+            font=ctk.CTkFont(size=20, weight="bold"),
+        ).pack(padx=20, pady=(20, 0))
+
+        suppression = self.rep.supprimer_contact(nom, prenom)
+
+        if not suppression:
+            label_text = f'Aucun contact "{prenom} {nom}" n\'a été trouvé.'
+        else:
+            label_text = f'Le contact "{prenom} {nom}" a bien été supprimé'
+
+        ctk.CTkLabel(fenetre_contacts, text=label_text).pack(padx=20, pady=10)
+
+        self.quitter_button(fenetre_contacts).pack(padx=20, pady=10)
+        fenetre_contacts.after(100, fenetre_contacts.lift)
+
+    def quitter_button(self, parent: ctk.CTk) -> ctk.CTkButton:
+        """
+        Crée un bouton 'Quitter' sur la fenêtre spécifiée.
+
+        Args:
+            parent (ctk.CTk): La fenêtre parente sur laquelle le bouton sera placé.
+
+        Returns:
+            ctk.CTkButton: Le bouton créé.
+        """
+        return ctk.CTkButton(parent, text="Quitter", command=parent.destroy)
--- repertoire.py
+++ repertoire.py
@@ -1,16 +1,25 @@
 import sqlite3
- 
+
+from interface import RepertoireUI
+
+
 class Repertoire:
     def __init__(self):
-        # Initialisation de la connexion à la base de données
+        """
+        Initialise la classe Repertoire en établissant une connexion à la base de données SQLite.
+        Prépare un curseur pour les opérations de base de données et initialise la structure de la base de données.
+        """
         self.conn = sqlite3.connect("contacts.sqlite")
         self.cur = self.conn.cursor()
+
         self.setup_database()
- 
+
     def setup_database(self):
-        # Création de la table des contacts si elle n'existe pas
-        self.cur.execute(
-            """
+        """
+        Crée la table 'contacts' dans la base de données si elle n'existe pas déjà.
+        Cette table est utilisée pour stocker les informations des contacts.
+        """
+        operation = """
             CREATE TABLE IF NOT EXISTS contacts (
                 id INTEGER PRIMARY KEY,
                 nom TEXT,
@@ -18,51 +27,99 @@ class Repertoire:
                 telephone TEXT
             )
         """
-        )
-        self.conn.commit()
- 
-    def ajouter_contact(self, nom, prenom, telephone):
-        # Ajout d'un nouveau contact
+
+        self.cur.execute(operation)
+
+    def run(self):
+        """
+        Lance l'interface utilisateur de l'application de répertoire et assure une sortie propre en appelant 'exit'.
+        """
+        try:
+            RepertoireUI(self)
+        finally:
+            self.exit()
+
+    def exit(self):
+        """
+        Ferme le curseur et la connexion à la base de données pour une fermeture propre des ressources de la base de données.
+        """
+        self.cur.close()
+        self.conn.close()
+
+    def ajouter_contact(self, nom: str, prenom: str, telephone: str):
+        """
+        Ajoute un nouveau contact à la base de données.
+
+        Args:
+            nom (str): Le nom de famille du contact.
+            prenom (str): Le prénom du contact.
+            telephone (str): Le numéro de téléphone du contact.
+        """
         self.cur.execute(
             "INSERT INTO contacts (nom, prenom, telephone) VALUES (?, ?, ?)",
             [nom, prenom, telephone],
         )
- 
+
         self.conn.commit()
- 
-    def supprimer_contact(self, nom, prenom):
-        # Suppression d'un contact existant
+
+    def supprimer_contact(self, nom: str, prenom: str) -> bool:
+        """
+        Supprime un contact existant de la base de données en fonction de son nom et prénom.
+
+        Args:
+            nom (str): Le nom de famille du contact à supprimer.
+            prenom (str): Le prénom du contact à supprimer.
+
+        Returns:
+            bool: True si le contact a été supprimé, False si aucun contact correspondant n'a été trouvé.
+        """
         self.cur.execute(
-            "SELECT id FROM contacts WHERE nom = ? AND prenom = ?", [nom, prenom]
+            "SELECT id FROM contacts WHERE nom LIKE ? AND prenom LIKE ?",
+            [f"%{nom}%", f"%{prenom}%"],
         )
- 
+
         contact = self.cur.fetchone()
+
         if not contact:
             return False
- 
- 
+
         self.cur.execute("DELETE FROM contacts WHERE id = ?", [contact[0]])
         self.conn.commit()
- 
+
         return True
- 
-    def afficher_contacts(self):
-        # Affichage de tous les contacts
+
+    def afficher_contacts(self) -> list[tuple]:
+        """
+        Récupère et retourne tous les contacts de la base de données, triés par nom puis par prénom.
+
+        Returns:
+            list[tuple]: Liste de tuples, chaque tuple contenant les informations d'un contact (nom, prenom, telephone).
+        """
         self.cur.execute(
             "SELECT nom, prenom, telephone FROM contacts ORDER BY nom, prenom"
         )
- 
-        contacts = self.cur.fetchall()
- 
-        return contacts
- 
-    def chercher_contact(self, nom: str):
-        # Recherche d'un contact
+
+        return self.cur.fetchall()
+
+    def chercher_contact(self, nom: str, prenom: str) -> list[tuple]:
+        """
+        Recherche des contacts dans la base de données par nom et prénom.
+
+        Args:
+            nom (str): Le nom à rechercher dans la base de données.
+            prenom (str): Le prénom à rechercher dans la base de données.
+
+        Returns:
+            list of tuples: Liste de tuples des contacts correspondants, chaque tuple contenant les informations (nom, prenom, telephone).
+        """
         self.cur.execute(
-            "SELECT nom, prenom, telephone FROM contacts WHERE nom LIKE ? OR prenom LIKE ?",
-            [f"%{nom}%", f"%{nom}%"],
+            "SELECT nom, prenom, telephone FROM contacts WHERE nom LIKE ? AND prenom LIKE ?",
+            [f"%{nom}%", f"%{prenom}%"],
         )
- 
-        resultats = self.cur.fetchall()
- 
-        return resultats
+
+        return self.cur.fetchall()
+
+
+if __name__ == "__main__":
+    app = Repertoire()
+    app.run()
